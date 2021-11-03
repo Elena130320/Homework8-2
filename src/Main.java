98f100297e0f95b0cc8f38281fb9c6d2ff006ed4
Elena <l.vmarte@yandex.ru>
2021-11-04 01:08:38 +0300

    ДЗ 8. Вторая часть

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,36 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        wordSet();
+
+    }
+
+    public static void wordSet() {
+        Random random = new Random();
+
+        String[] wordArr = new String[] {"apple", "orange", "lemon", "banana",
+                "apricot", "avocado", "broccoli", "carrot", "cherry", "garlic",
+                "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut",
+                "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
+
+        int index = random.nextInt(wordArr.length);
+        String word = wordArr[index];
+        System.out.println("Загаданное слово - " + word);
+
+
+        String answerWord;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            answerWord = scanner.nextLine();
+            if (answerWord.equals(word)) {
+                System.out.println("Верно!");
+            }
+            else {
+                System.out.println("Попробуйте еще раз");
+            }
+        } while (answerWord.equals(word));
+
+    }
 }
